6731158f312da979f88345635a18c551ba075bbe
Tal <tal.bny3@gmail.com>
2020-02-29 16:55:19 +0200

    fixed the same bug, but in /login

--- backend/routes/testApi.js
+++ backend/routes/testApi.js
@@ -9,70 +9,74 @@ var Schema = mongoose.Schema;
 var usersModel = mongoose.model('users', {
     username: String,
     password: String
-  });
+});
 
 
 
-router.get('/users', function(request, response){
-    usersModel.findOne({'username':'Daasdn'},'username password',function (err, users)  {
+router.get('/users', function (request, response) {
+    usersModel.findOne({ 'username': 'Daasdn' }, 'username password', function (err, users) {
         if (users) {
-            return response.json({password:users.password});    
+            return response.json({ password: users.password });
         } else {
-            return response.json({error: "username does not exists"});    
+            return response.json({ error: "username does not exists" });
         }
     });
 
 
-    
+
 });
 
-router.post('/register', function(request, response, next){
+router.post('/register', function (request, response, next) {
     var username = request.body.username;
     var password = request.body.password;
-    
+
     if (!username || !password) {
         return response.status(400).send('Missing fields')
     }
 
-    usersModel.findOne({'username': username},'username password',function (err, users)  {
+    usersModel.findOne({ 'username': username }, 'username password', function (err, users) {
         if (users) {
             return response.status(400).send('username already existed. try another one')
         } else {
             var cryptedPassword = crypto.createHash('sha256').update(password).digest('base64');
-            var newUser = [{'username': username, 'password': cryptedPassword}]
-            usersModel.insertMany(newUser, function(err, res){
+            var newUser = [{ 'username': username, 'password': cryptedPassword }]
+            usersModel.insertMany(newUser, function (err, res) {
                 if (err) {
                     console.log(err)
-                    return response.status(500).send('an error occured') 
+                    return response.status(500).send('an error occured')
                 } else {
-                    return response.status(500).send('register successfully') 
+                    return response.status(500).send('register successfully')
                 }
 
             })
         }
-    });    
+    });
 });
 
-router.post('/login', function(request, response, next) {
+router.post('/login', function (request, response, next) {
     var username = request.body.username;
     var password = request.body.password;
-    
+
     if (!username || !password) {
         return response.status(400).send('Missing fields')
     }
 
     var cryptedPassword = crypto.createHash('sha256').update(password).digest('base64');
 
-    usersModel.findOne({'username': username},'username password',function (err, users)  {
-        if (cryptedPassword == users.password) {
-            return response.send('You are real');
+    usersModel.findOne({ 'username': username }, 'username password', function (err, users) {
+        if (users) {
+            if (cryptedPassword == users.password) {
+                return response.send('You are real');
+            } else {
+                return response.send('You arnt real');
+            }
         } else {
-            return response.send('You arnt real');
-        } 
+            return response.send('username does not exist');
+        }
     });
 
 
 
 });
 
-module.exports=router;
+module.exports = router;
